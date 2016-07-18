0cced74ffb648593ca4a0ba4abb17cb81011ea87
safwaf <safwafsafwaf@gmail.com>
2016-07-18 15:40:36 -0600

    overworld constructor now initializes the biome grid to 0s and picks 8 biomes for the map

--- dungeonerator/overworld.h
+++ dungeonerator/overworld.h
@@ -5,8 +5,10 @@ public:
 	overworld();
 	~overworld();
 private:
+	int mapwidth;
+	int mapheight;
 	int biomeGrid[16][16]; //the map of which tiles are part of which biomes
 	int biomeNumber; //how many biomes this map will contain
-	bool biomelist[16]; //a t/f list indicating for each biome whether it is in the map
+	bool biomeList[16]; //a t/f list indicating for each biome whether it is in the map
 };
 
