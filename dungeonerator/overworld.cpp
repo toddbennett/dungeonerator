0aa16529d3fe03c2d83d7447d83ff43ca5a70d0e
safwaf <safwafsafwaf@gmail.com>
2016-07-18 21:45:10 -0600

    wrote map assigner prototype for volcano biome

--- dungeonerator/overworld.cpp
+++ dungeonerator/overworld.cpp
@@ -9,6 +9,7 @@ overworld::overworld()
 	mapheight = 16;
 	biomeNumber = 8; 
 
+	mapsize = mapwidth*mapheight;
 
 	for (int i=0;i<16;i++) //fill the array with 0s
 	{ 
@@ -32,6 +33,73 @@ overworld::overworld()
 			biomeList[i] = true;
 		}
 		else biomeList[i] = false;
+	}
+	
+	//start placing the biomes we have picked
+	//place mountains first
+	if (biomeList[4])
+	{
+		point node;
+		if (rand() % 2 == 1) node = { 0,0 };
+		else node = { 15,0 };
+
+		biomeGrid[node.x][node.y] = 4;
+		int biomesize = 20 + (rand() % 10) + (rand() % 10); //random value between 20 and 40, likely to be close to 30
+		int a = 1; //number of points we have assigned to the biome so far
+		point biomePoints[40];
+		while (a < biomesize)
+		{
+			for (int i = 0; i < a; i++)
+			{
+				//check the points adjacent to biomePoints[a] could be added to the biome. if so, has a chance to add them
+				//left point
+				if (biomePoints[a].x > 0)
+				{
+					if (biomeGrid[biomePoints[a].x - 1][biomePoints[a].y] == 0 && rand() % 2 == 1)
+					{
+						biomeGrid[biomePoints[a].x - 1][biomePoints[a].y] = 4;
+						a++;
+						if (a == biomesize) break;
+					}
+				}
+				//right point
+				if (biomePoints[a].x < 15)
+				{
+					if (biomeGrid[biomePoints[a].x + 1][biomePoints[a].y] == 0 && rand() % 2 == 1)
+					{
+						biomeGrid[biomePoints[a].x + 1][biomePoints[a].y] = 4;
+						a++;
+						if (a == biomesize) break;
+					}
+				}
+				//up point
+				if (biomePoints[a].y > 0)
+				{
+					if (biomeGrid[biomePoints[a].x][biomePoints[a].y - 1] == 0 && rand() % 2 == 1)
+					{
+						biomeGrid[biomePoints[a].x][biomePoints[a].y - 1] = 4;
+						a++;
+						if (a == biomesize) break;
+					}
+				}
+				//down point
+				if (biomePoints[a].y < 15)
+				{
+					if (biomeGrid[biomePoints[a].x][biomePoints[a].y + 1] == 0 && rand() % 2 == 1)
+					{
+						biomeGrid[biomePoints[a].x][biomePoints[a].y + 1] = 4;
+						a++;
+						if (a == biomesize) break;
+					}
+				}
+			}
+		}
+	}
+	if (biomeList[7])
+	{
+		point node;
+		node.x = rand() % 16;
+		node.y = rand() % 16;
 	}
 }
 
--- dungeonerator/overworld.h
+++ dungeonerator/overworld.h
@@ -1,4 +1,10 @@
 #pragma once
+
+struct point {
+	int x;
+	int y;
+};
+
 class overworld
 {
 public:
@@ -7,6 +13,7 @@ public:
 private:
 	int mapwidth;
 	int mapheight;
+	int mapsize;
 	int biomeGrid[16][16]; //the map of which tiles are part of which biomes
 	int biomeNumber; //how many biomes this map will contain
 	bool biomeList[16]; //a t/f list indicating for each biome whether it is in the map
