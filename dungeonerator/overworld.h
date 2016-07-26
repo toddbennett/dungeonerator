3351380ae6f0e618d92879de3cdc8fe0d8c87c9b
Todd Bennett <xtoddxiferx@hotmail.com>
2016-07-26 13:21:17 -0600

    refactored overworld to not use as many magic numbers

--- dungeonerator/overworld.h
+++ dungeonerator/overworld.h
@@ -13,13 +13,13 @@ public:
 	~overworld();
 	biomeType getBiome(int x, int y);
 private:
-	void growPoint(int biome, point nodule, int biomesize, int percentage);
-	void fillPoint(int biome);
+	void growPoint(biomeType biome, point nodule, int biomesize, int percentage);
+	void fillPoint(biomeType biome);
 	point getFreePoint();
 	int mapwidth;
 	int mapheight;
 	int mapsize;
-	int biomeGrid[16][16]; //the map of which tiles are part of which biomes
+	biomeType biomeGrid[16][16]; //the map of which tiles are part of which biomes
 	int biomeNumber; //how many biomes this map will contain
 	bool biomeList[16]; //a t/f list indicating for each biome whether it is in the map
 };
